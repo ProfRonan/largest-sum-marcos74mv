3ed64fdbac9e44852b154525148d3f1e962060c6
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-05-24 13:34:09 +0000

    maior soma

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 """Module with functions."""
 
-
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
-    return primeiro, segundo
+    if len(numbers) < 2:
+        return None
+    
+    sorted_numbers = sorted(numbers, reverse=True)
+    primeiro = sorted_numbers[0]
+    segundo = sorted_numbers[1]
+    
+    return (min(primeiro, segundo), max(primeiro, segundo))
+
+testes1 = [
+    [1, 2, 3, 4, 5],
+    [5, 4, 3, 2, 1],
+    [10, -5, 20, 15, -30],
+    [0, 0, 0, 0],
+    [-10, -5, -2, -1],
+    [5],
+    []
+]
+
+for i, teste2 in enumerate(testes1):
+    print(largest_sum(teste2))
+    print()
